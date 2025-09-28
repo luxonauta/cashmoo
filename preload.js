dc7672d68b599e48709527566bce62a66372ca24
luxonauta <luxon4uta@gmail.com>
2025-09-28 09:46:45 -0300

    🔧 refactor import statement to use require syntax

--- preload.js
+++ preload.js
@@ -1,4 +1,4 @@
-import { contextBridge, ipcRenderer } from "electron";
+const { contextBridge, ipcRenderer } = require("electron");
 
 contextBridge.exposeInMainWorld("cashmoo", {
   setupSave: (payload) => ipcRenderer.invoke("setup:save", payload),
